851e715bd94aca4007555a5c201e8ca7e60531d7
pratham-sarankar <pratham@sarankar.com>
2026-06-20 10:06:20 +0530

    2nd commit

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("Hello, SRGEC!");
+console.log("Hello, SRGEC!");
+
+// 1. Make some changes in a file
+// 2. Add the changes to the staging area - git add .
+// 3. Commit the changes - git commit -m "Your commit message"
+// 4. Log the commit history - git log
