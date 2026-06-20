5173ea9e3126a679e0bb8cc9681f03b92dbca168
pratham-sarankar <pratham@sarankar.com>
2026-06-20 10:27:18 +0530

    new branch commit

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ console.log("Hello, SRGEC!");
 // 2. Add the changes to the staging area - git add .
 // 3. Commit the changes - git commit -m "Your commit message"
 // 4. Log the commit history - git log
+
+// New branch change.
